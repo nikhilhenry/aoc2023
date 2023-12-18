1e153ed313bd23381e267304b2260626417df330
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-18 13:18:29 +0530

    correct date

--- get_data.sh
+++ get_data.sh
@@ -6,4 +6,4 @@ then
     echo "day number is not specified"
     exit 1
 fi
-curl -b "session=${cookie}" "https://adventofcode.com/2022/day/${1}/input" > "./data/day${1}.input"
+curl -b "session=${cookie}" "https://adventofcode.com/2023/day/${1}/input" > "./data/day${1}.input"
