903d926a64ff13b19eed781dcd146583aa28fff5
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-22 22:42:41 +0530

    refactor

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,6 +2,12 @@ use anyhow::Result;
 use itertools::Itertools;
 use std::iter::zip;
 
+fn count_distance(time: usize, dist: usize) -> usize {
+    let speeds = (1..time).collect_vec();
+    let distances = speeds.iter().map(|speed| (time - speed) * speed);
+    distances.filter(|d| *d > dist).count()
+}
+
 fn main() -> Result<()> {
     let mut data = include_str!("../../data/day6.input").split("\n");
     let times = data
@@ -16,13 +22,9 @@ fn main() -> Result<()> {
         .filter_map(|s| s.parse::<usize>().ok());
 
     let count: usize = zip(times, distances)
-        .map(|(time, dist)| {
-            let speeds = (1..time).collect_vec();
-            let distances = speeds.iter().map(|speed| (time - speed) * speed);
-            distances.filter(|d| *d > dist).count()
-        })
+        .map(|(time, dist)| count_distance(time, dist))
         .product();
-    println!("{:?}", count);
+    println!("Part 1: {:?}", count);
 
     Ok(())
 }
