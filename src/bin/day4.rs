76b82f1e12e217d90200188bd69fdfc7ced91c46
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-22 15:22:00 +0530

    complete part 1

---
+++ src/bin/day4.rs
@@ -0,0 +1,63 @@
+use std::str::FromStr;
+
+use anyhow::{anyhow, Error, Result};
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    winning_nums: Vec<usize>,
+    nums: Vec<usize>,
+}
+
+impl Card {
+    fn points(&self) -> usize {
+        let num_winning = self
+            .nums
+            .iter()
+            .filter(|num| self.winning_nums.contains(num))
+            .count();
+
+        if num_winning > 0 {
+            return 2_usize.pow((num_winning - 1) as u32);
+        }
+        0
+    }
+}
+
+impl FromStr for Card {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (idx, lists) = s.split_once(":").ok_or(anyhow!("invalid input"))?;
+        let (_, idx) = idx.split_once(" ").unwrap();
+        let id = idx.trim().to_string().parse().expect("idx not valid ");
+
+        let (winning_nums, nums) = lists.trim().split_once("|").unwrap();
+
+        let winning_nums = winning_nums
+            .split(" ")
+            .filter_map(|num| num.parse::<usize>().ok())
+            .collect_vec();
+
+        let nums = nums
+            .split(" ")
+            .filter_map(|num| num.parse::<usize>().ok())
+            .collect_vec();
+
+        Ok(Card {
+            id,
+            winning_nums,
+            nums,
+        })
+    }
+}
+
+fn main() -> Result<()> {
+    let cards = aoc::read_one_per_line::<Card>("./data/day4.input")?;
+    println!(
+        "Part 1: {:?}",
+        cards.iter().map(|card| card.points()).sum::<usize>()
+    );
+    Ok(())
+}
