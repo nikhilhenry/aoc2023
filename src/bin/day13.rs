b5fed25735f53662782c74a45c2b11e3863b2b43
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-24 15:26:09 +0530

    close but not enough

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,5 +1,129 @@
+use std::{cmp, usize};
+
 use anyhow::Result;
+use aoc::{Grid, Position};
+use itertools::Itertools;
+
+fn collect_to_num(grid: &Grid<char>, poses: Vec<Position>) -> usize {
+    poses
+        .iter()
+        .map(|pos| if grid.get(&pos) == &'#' { '1' } else { '0' })
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap()
+}
+
+fn collect_row(grid: &Grid<char>, row: usize) -> usize {
+    let poses = (0..grid.cols).map(|col| aoc::pos!(row, col)).collect_vec();
+    collect_to_num(grid, poses)
+}
+
+fn collect_col(grid: &Grid<char>, col: usize) -> usize {
+    let poses = (0..grid.rows).map(|row| aoc::pos!(row, col)).collect_vec();
+    collect_to_num(grid, poses)
+}
+
+#[derive(Debug)]
+enum Reflection {
+    Vertical(usize),
+    Horizontal(usize),
+}
+
+struct PatternGrid {
+    cols: Vec<usize>,
+    rows: Vec<usize>,
+}
+
+impl PatternGrid {
+    fn check_reflection(&self, check_row: bool, idxs: (usize, usize)) -> Option<usize> {
+        let items = if check_row { &self.rows } else { &self.cols };
+        let len = items.len();
+        if idxs.0 - 1 == 0 || idxs.1 - 1 == len {
+            return None;
+        }
+        let row_top = idxs.0 - 1;
+        let row_bottom = idxs.1 - 1;
+        let mut idx = 0;
+        //let target_idx = cmp::min(row_top, len - row_bottom - 1);
+        let target_idx = row_top;
+        //println!("target: {target_idx}");
+        while idx != target_idx {
+            let Some(r1) = items.get(row_top - idx) else {
+                return None;
+            };
+            let Some(r2) = items.get(row_bottom + idx) else {
+                return None;
+            };
+            if r1 == r2 {
+                idx += 1
+            } else {
+                return None;
+            }
+        }
+        Some(row_top + 1)
+    }
+
+    fn find_reflection(&self) -> Option<Reflection> {
+        // try to find vertical reflections
+        let mut raw = (0..self.cols.len()).collect_vec();
+        if raw.len() % 2 == 1 {
+            raw.pop();
+        }
+        let idxs = raw.windows(2).map(|pair| (pair[0] + 1, pair[1] + 1));
+        let mut matches = idxs.filter_map(|pair| self.check_reflection(false, pair));
+        if let Some(num) = matches.next() {
+            return Some(Reflection::Vertical(num));
+        }
+
+        // try to find horizontal reflections
+        let mut raw = (0..self.rows.len()).collect_vec();
+        if raw.len() % 2 == 1 {
+            raw.pop();
+        }
+        let idxs = raw.windows(2).map(|pair| (pair[0] + 1, pair[1] + 1));
+        let mut matches = idxs.filter_map(|pair| self.check_reflection(true, pair));
+        if let Some(num) = matches.next() {
+            return Some(Reflection::Horizontal(num));
+        }
+        None
+    }
+}
 
 fn main() -> Result<()> {
+    let mut data = include_str!("../../data/day13.input").to_string();
+    data.pop();
+    let data = data.split("\n\n");
+    let grids = data.filter_map(|s| {
+        let mut s = s.to_string();
+        s.push('\n');
+        s.parse::<Grid<char>>().ok()
+    });
+
+    let mut grids = grids.map(|g| PatternGrid {
+        cols: (0..g.cols).map(|col| collect_col(&g, col)).collect(),
+        rows: (0..g.rows).map(|row| collect_row(&g, row)).collect(),
+    });
+
+    let score: usize = grids
+        .filter_map(|g| g.find_reflection())
+        .map(|reflection| match reflection {
+            Reflection::Vertical(num) => num,
+            Reflection::Horizontal(num) => num * 100,
+        })
+        .sum();
+    println!("Part 1: {score}");
+
+    todo!();
+
+    println!("{:?}", grids.next().unwrap().find_reflection());
+    println!("{:?}", grids.next().unwrap().find_reflection());
+
+    todo!();
+
+    let first_col_ref = grids.next().unwrap().check_reflection(false, (5, 6));
+    println!("{:?}", first_col_ref);
+    let first_row_ref = grids.next().unwrap().check_reflection(true, (4, 5));
+    println!("{:?}", first_row_ref);
+
     Ok(())
 }
